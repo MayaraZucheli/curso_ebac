45342f86188f1e73113bd26af2019dd6fc797b05
MayaraZucheli <131776167+MayaraZucheli@users.noreply.github.com>
2024-02-08 22:19:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+
+console.log(multiplicacao(5, 3)); 
+console.log(saudacao("Mayara")); 

3372a370dd70347959381cd2de93a90a7d48aef7
MayaraZucheli <131776167+MayaraZucheli@users.noreply.github.com>
2024-04-14 21:32:53 -0300

    Add files via upload

---
+++ src/MediaNotas.java
@@ -0,0 +1,12 @@
+public class MediaNotas {
+    public static void main(String[] args) {
+        double nota1 = 10;
+        double nota2 = 9;
+        double nota3 = 8;
+        double nota4 = 7;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.printf("A média das notas é: %.2f\n", media);
+    }
+}

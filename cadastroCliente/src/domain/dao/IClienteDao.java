10b8ab580d89caa833376ebd0d64809d63b0a942
Mayara Zucheli <131776167+MayaraZucheli@users.noreply.github.com>
2024-07-01 13:49:13 -0300

    Add files via upload

---
+++ cadastroCliente/src/domain/dao/IClienteDao.java
@@ -0,0 +1,19 @@
+package domain.dao;
+
+import java.util.Collection;
+
+import domain.Cliente;
+
+public interface IClienteDao {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+
+}

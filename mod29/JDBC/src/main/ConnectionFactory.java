596f2031313d859a330dfffbb64214a475d72e83
Mayara Zucheli <131776167+MayaraZucheli@users.noreply.github.com>
2024-09-05 13:42:15 -0300

    Add files via upload

---
+++ mod29/JDBC/src/main/ConnectionFactory.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	private static Connection connection;
+	
+	private ConnectionFactory(Connection connection) {
+		
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		if (connection == null) {
+			connection = initConnection();
+		} else if (connection != null && connection.isClosed()) {
+			connection = initConnection();
+		}
+		return connection;
+	}
+	
+	private static Connection initConnection() {
+		try {
+			return DriverManager.getConnection(
+					"jdbc:postgresql://localhost:15432/mod_29", "postgres", "admin");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ mod29/JDBC/src/main/dao/iClienteDAO.java
@@ -0,0 +1,18 @@
+package main.dao;
+
+import java.util.List;
+
+import domain.Cliente;
+
+public interface iClienteDAO {
+	
+	public Integer cadastrar(Cliente cliente) throws Exception;
+	
+	public Integer atualizar(Cliente cliente) throws Exception;
+
+	public Cliente buscar(String codigo) throws Exception;
+	
+	public List<Cliente> buscarTodos() throws Exception;
+	
+	public Integer excluir(Cliente cliente) throws Exception;
+}
